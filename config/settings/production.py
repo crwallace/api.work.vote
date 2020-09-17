b21ad57a9cd14260b19939b0a854c6288b603490
Magneton <mlong@magneton.io>
2020-09-17 03:41:51 -0500

    settings update

--- config/settings/production.py
+++ config/settings/production.py
@@ -63,7 +63,9 @@ AWS_HEADERS = {
 }
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+
 STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+
 # END STORAGE CONFIGURATION
 
 
@@ -86,3 +88,4 @@ CACHES = {
         }
     }
 }
+
